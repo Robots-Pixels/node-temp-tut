cd2a59e5ca74faf75cfb1988fc442f54eb515069
Robots-Pixels <robotspixels@gmail.com>
2024-11-22 18:15:03 +0100

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,11 @@
+const names = require("./4-names")
+console.log(require("./4-names"))
+
+const sayHi = require("./5-utils")
+const flavorData = require("./6-alternative-flavour")
+
+require("./7-mind-grenade")
+
+sayHi('susan')
+sayHi(names.john)
+sayHi(names.peter)
